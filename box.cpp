5e9077ed4a10b7454744f6a0dbd1f522afa57bb9
Alla <110739884+AllaAndreevna@users.noreply.github.com>
2024-10-19 11:48:57 +0300

    Laboratory 2. corrected

--- box.cpp
+++ box.cpp
@@ -1,71 +1,48 @@
 // реализация методов, объявленных в заголовочном файле .h
-class GameBox {
-private:
-    std::string name_; 
-    int width_; 
-    int height_; 
-    int depth_; 
+#include "box.h"
 
-public:
+// Реализация методов класса GameBox
 
-    GameBox(const std::string& name, int width, int height, int depth)
-        : name_(name), width_(width), height_(height), depth_(depth) {}
+// Конструктор инициализации
+GameBox::GameBox(const std::string& name, int width, int height, int depth)
+    : Box(width, height, depth), name_(name) {}
 
-    // Методы доступа
-    std::string getName() const { return name_; }
-    int getWidth() const { return width_; }
-    int getHeight() const { return height_; }
-    int getDepth() const { return depth_; }
+// Метод для получения имени
+std::string GameBox::getName() const {
+    return name_;
+}
 
-    // Метод проверки пересечения с другими коробками
-    bool intersects(const GameBox& other) const {
-        // for sкоробки пересекаются, если они "накладываются" друг на друга в любом измерении (width, height, depth)
-        return (width_ > other.width_ && height_ > other.height_ && depth_ > other.depth_) ||
-               (width_ < other.width_ && height_ < other.height_ && depth_ < other.depth_);
-    }
-};
+// Метод для проверки пересечения с другими коробками
+bool GameBox::intersects(const GameBox& other) const {
+    // Коробки пересекаются, если они "накладываются" друг на друга в любом измерении
+    return (width_ > other.width_ && height_ > other.height_ && depth_ > other.depth_) ||
+           (width_ < other.width_ && height_ < other.height_ && depth_ < other.depth_);
+}
 
+// Реализация методов класса BoxContainer
 
-class BoxContainer {
-private:
-    std::vector<GameBox> boxes_; 
-    int maxItems_; 
-    int width_;
-    int height_; 
-    int depth_; 
+// Конструктор инициализации
+BoxContainer::BoxContainer(int width, int height, int depth, int maxItems)
+    : Box(width, height, depth), maxItems_(maxItems) {}
 
-public:
-
-    BoxContainer(int width, int height, int depth, int maxItems)
-        : width_(width), height_(height), depth_(depth), maxItems_(maxItems) {}
-
-    // Методы доступа
-    int getMaxItems() const { return maxItems_; }
-    int getWidth() const { return width_; }
-    int getHeight() const { return height_; }
-    int getDepth() const { return depth_; }
-    std::vector<GameBox> getBoxes() const { return boxes_; }
-
-    // Метод добавления коробки в контейнер
-    bool addBox(const GameBox& box) {
-        if (boxes_.size() >= maxItems_) {
-            return false; // container is full
-        }
-
-        // проверка: можем ли поместить коробку
-        if (box.getWidth() > width_ || box.getHeight() > height_ || box.getDepth() > depth_) {
-            return false; // box does not fit
-        }
-
-        // проверка на пересечения с другими коробками
-        for (const auto& existingBox : boxes_) {
-            if (box.intersects(existingBox)) {
-                return false; // коробка пересекается 
-            }
-        }
-
-        boxes_.push_back(box);
-        return true; // коробка добавлена
+// Метод добавления коробки в контейнер
+bool BoxContainer::addBox(const GameBox& box) {
+    if (boxes_.size() >= maxItems_) {
+        return false; // контейнер полон
     }
 
-};
+    // Проверка: можем ли поместить коробку
+    if (box.getWidth() > width_ || box.getHeight() > height_ || box.getDepth() > depth_) {
+        return false; // коробка не помещается
+    }
+
+    // Проверка на пересечения с другими коробками
+    for (const auto& existingBox : boxes_) {
+        if (box.intersects(existingBox)) {
+            return false; // коробка пересекается
+        }
+    }
+
+    boxes_.push_back(box);
+    return true; // коробка добавлена
+}
